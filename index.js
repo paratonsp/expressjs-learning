42daed60e29b6ed1723c3fe863622b3cd609826b
Paraton Suryo Prayogo <70687618+paratonsp@users.noreply.github.com>
2023-07-08 01:49:33 +0700

    errRes

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const app = express();
 const port = process.env.PORT || 8080; 
 
 app.get('/', (req, res) => {
-    res.status(200);
-    res.send('expressjs rest api');
+    res.status(200).json({success: true, message: 'expressjs-learning'});
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 400).json({
+        success: false,
+        message: err.message || 'An error occured.',
+        errors: err.error || [],
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).json({ success: false, message: 'Resource not found.' });
 });
 
 app.listen(port, () => {
